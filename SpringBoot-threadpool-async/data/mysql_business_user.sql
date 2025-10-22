d4d45998743fd1bb433df5b1a3a324e56c40a348
yangwneely <wneelyyang@gmail.com>
2025-10-22 17:33:31 +0800

    Add files via upload

---
+++ SpringBoot-threadpool-async/data/mysql_business_user.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `business_user` (
+  `name` varchar(200) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `id_no` varchar(20) DEFAULT NULL,
+  `zh_code` varchar(200) DEFAULT NULL,
+  `kch_code` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
